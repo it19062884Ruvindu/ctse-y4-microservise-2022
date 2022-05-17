643f80c27da146c2d458715548a0e939a3e3321c
it19062884Ruvindu <it19062884@my.sliit.lk>
2022-05-17 15:48:51 +0530

    simple change two

--- server.js
+++ server.js
@@ -4,8 +4,7 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
-
-
+//my micro service rout
 const SiteRoute = require('./src/routes/route.site');
 
 
@@ -13,9 +12,6 @@ const SiteRoute = require('./src/routes/route.site');
 dotenv.config();
 const app = express();
 app.use(cors());
-
-
-//
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
@@ -34,7 +30,6 @@ mongoose.connect('mongodb+srv://user-123:user-123@construction-management.sk0gl.
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }, (error) => {
-
     if (error) {
         console.log('Database Error:', error.message);
         console.log('######################################################');
@@ -43,18 +38,15 @@ mongoose.connect('mongodb+srv://user-123:user-123@construction-management.sk0gl.
 
 mongoose.connection.once('open', () => {
     console.log('Database Connected...');
-    // initial();
     console.log('######################################################');
 });
 
 app.route('/').get((req, res) => {
-    res.send('MicroService-01 run (Backend by :- Ruvindu Kaushalya)');
-});
+    res.send('MicroService-01, __Product Distributing Site Managemt Backend__* Running with all Configurations !! (Pipe line, GIT, GCP, Docker, Kubernets )');
+  //  res.send('Git updated, Pipeline fired, docarizing, containarizing , loded on GCP, Genarated by Public IP with Kubernet \n');
+  //  res.send('(Backend by :- Ruvindu Kaushalya , CTSE Assignmet-03)');
 
-
-// require('./src/routes/auth.routes')(app);
-// require('./src/routes/user.routes')(app);
-
+  });
 
 
 app.use('/site', SiteRoute());
@@ -64,24 +56,3 @@ app.listen(PORT, () => {
     console.log(`Server is ON and running on PORT : ${PORT}`);
     console.log('...Wait DB connecting...');
 });
-
-// const db = require("./src/modules");
-// const Role = db.role;
-
-
-//   function initial() {
-//     Role.estimatedDocumentCount((err, count) => {
-//       if (!err && count === 0) {
-//         new Role({
-//           name: "user"
-//         }).save(err => {
-//           if (err) {
-//             console.log("error", err);
-//           }
-  
-//           console.log("added 'user' to roles collection");
-//          console.log('######################################################');
-//         });
-//       }
-//     });
-//   }
